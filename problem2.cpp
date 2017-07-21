9859a346668b81bda40934087dd69bb29de67ca4
boltzbo <chakraphan.choochot@gmail.com>
2017-07-21 17:11:07 +0700

    Create a struct to hold values

--- problem2.cpp
+++ problem2.cpp
@@ -1,4 +1,5 @@
 // g++ problem2.cpp -std=c++1y
+// git push -u origin master
 
 #include <string>
 #include <sstream>
@@ -11,11 +12,23 @@
 
 using namespace std;
 
+struct STATISTIC_DATA {
+  int min;
+  int max;
+  int count;
+  int sum;
+  float avg;
+};
+
 int main() {
-  int count = 0, sum = 0, min = INT_MAX, max = INT_MIN;
-  string line;
+  STATISTIC_DATA stat;
+  stat.count = 0;
+  stat.sum = 0;
+  stat.min = INT_MAX;
+  stat.max = INT_MIN;
 
   ifstream input("problem2.input");
+  string line;
   while (getline(input, line)) {
     istringstream iss(line.c_str());
     int val;
@@ -23,20 +36,20 @@ int main() {
       break;
     }
 
-    ++count;
-    sum += val;
-    min = std::min(min, val);
-    max = std::max(max, val);
+    ++stat.count;
+    stat.sum += val;
+    stat.min = std::min(stat.min, val);
+    stat.max = std::max(stat.max, val);
   }
   input.close();
 
-  float avg = float(sum)/float(count);
+  stat.avg = float(stat.sum) / float(stat.count);
 
-  cout << "count: " << count << endl;
-  cout << "sum: " << sum << endl;
-  cout << "min: " << min << endl;
-  cout << "max: " << max << endl;
-  cout << "avg: " << avg << endl;
+  cout << "stat.count: " << stat.count << endl;
+  cout << "stat.sum: " << stat.sum << endl;
+  cout << "stat.min: " << stat.min << endl;
+  cout << "stat.max: " << stat.max << endl;
+  cout << "stat.avg: " << stat.avg << endl;
 
   return 0;
 }
